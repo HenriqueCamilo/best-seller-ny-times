8703546001d92e11ec2f8a058bccd16ed4829a45
Henrique <henriquecamilomapa@gmail.com>
2020-08-23 20:54:22 -0300

    first commit

---
+++ app/src/main/java/com/example/nybooks/presentation/books/BooksActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nybooks.presentation.books
+
+import android.example.nybooks.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BooksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
