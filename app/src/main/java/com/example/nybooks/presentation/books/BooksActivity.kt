9fa1eda99082bd49e3ef7f16aef25dc0628349c5
Henrique <henriquecamilomapa@gmail.com>
2020-08-23 21:30:32 -0300

    1

--- app/src/main/java/com/example/nybooks/presentation/books/BooksActivity.kt
+++ app/src/main/java/com/example/nybooks/presentation/books/BooksActivity.kt
@@ -3,10 +3,15 @@ package com.example.nybooks.presentation.books
 import android.example.nybooks.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_books.*
 
 class BooksActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_books)
+
+        toolbarMain.title = "Books"
+        setSupportActionBar(toolbarMain)
+
     }
 }
