28c440c19ea8a30319678cee4dbe9d45b2e24532
Henrique <henriquecamilomapa@gmail.com>
2020-08-24 14:07:17 -0300

    Create adapter

--- app/src/main/java/com/example/nybooks/presentation/books/BooksActivity.kt
+++ app/src/main/java/com/example/nybooks/presentation/books/BooksActivity.kt
@@ -3,6 +3,9 @@ package com.example.nybooks.presentation.books
 import android.example.nybooks.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.nybooks.data.model.Book
 import kotlinx.android.synthetic.main.activity_books.*
 
 class BooksActivity : AppCompatActivity() {
@@ -10,8 +13,21 @@ class BooksActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_books)
 
-        toolbarMain.title = "Books"
+        toolbarMain.title = getString(R.string.books_title)
         setSupportActionBar(toolbarMain)
 
+        with(recyclerBooks){
+            adapter = BooksAdapter(getBooks())
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this@BooksActivity, RecyclerView.VERTICAL, false)
+        }
+    }
+
+    fun getBooks(): List<Book>{
+        return listOf(
+            Book("title 1", "Author 1"),
+            Book("title 2", "Author 2"),
+            Book("title 3", "Author 3")
+        )
     }
 }
